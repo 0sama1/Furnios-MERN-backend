579cd918d126b15cd7fa61c97ed4a1337547060a
unknown <ossama19992010@hotmail.com>
2023-11-20 14:19:23 +0300

    product model and category model with a relationship added. category route adedd, GET all categories, POST a category

--- src/models/user.ts
+++ src/models/user.ts
@@ -2,16 +2,38 @@ import mongoose from 'mongoose'
 
 
 const userSchema = new mongoose.Schema({
-  name: {
+  firstName: {
     type: String,
     required: true,
+  },
+  lasttName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  isBlocked:{
+    type: Boolean,
   },
   // relation between order and user should be many orders to one user
   // here's 1to1 just for the demo
   order: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: [mongoose.Schema.Types.ObjectId],
     ref: 'Order',
   },
 })
 
-export default mongoose.model('Client', userSchema)
+export default mongoose.model('User', userSchema)
---
+++ src/routers/category.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+const router = express.Router()
+
+import Category from '../models/category'
+import ApiError from '../errors/ApiError'
+
+router.get('/', async (_, res) => {
+  const categories = await Category.find()
+  console.log('categories:', categories)
+  res.json(categories)
+})
+
+router.post('/', async (req, res, next) => {
+  const { name, image, description, categories, variants, sizes, price, quantity } = req.body
+
+  if (!name) {
+    next(ApiError.badRequest('Name are requried'))
+    return
+  }
+  const category = new Category({
+    name,
+  })
+
+  await category.save()
+  res.json(category)
+})
+
+export default router
