f0a3cec0876a68b6faedfa1e2da6ed9b6e349d1e
unknown <ossama19992010@hotmail.com>
2023-11-22 21:50:25 +0300

    some bugs solved

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -7,6 +7,12 @@ import ApiError from '../errors/ApiError'
 
 // get all products
 
+router.get('/', async (req, res) => {
+  const page= Number(req.query.page) || 1
+  const perPage = Number(req.query.perPage) || 3
+  const products = await Product.find().skip((page - 1) * perPage).limit(perPage).populate('categories')
+  const totalItems= await Product.countDocuments()
+  const totalPages = Math.ceil(totalItems / perPage)
   console.log('products:', products)
   res.json({
     totalItems,
@@ -25,21 +31,12 @@ router.get('/:productId', async (req, res, next) => {
   }
   res.json(product)
 })
-// get single product by id
-router.get('/:productId', async (req, res, next) => {
-  const productId = req.params.productId
-  const product = await Product.findById(productId)
-  if(!product){
-    next(ApiError.badRequest('Product not found!'))
-  }
-  res.json(product)
-})
 
 router.post('/', async (req, res, next) => {
   const { name, image, description, categories, variants, sizes, price, quantity } = req.body
 
   if (!name || !description || !price) {
-    next(ApiError.badRequest('Name, Description and Price are requried .'))
+    next(ApiError.badRequest('Name, Description and Price are requried'))
     return
   }
   const product = new Product({
@@ -56,7 +53,7 @@ router.post('/', async (req, res, next) => {
   await product.save()
   res.json(product)
 })
-// Delete a product by its ID
+// Delete a product by its ID.
 router.delete('/:productId', async (req, res, next) => {
   const productId = req.params.productId
   const product =await Product.deleteOne({
@@ -69,5 +66,50 @@ router.delete('/:productId', async (req, res, next) => {
 
 })
 // update a product by its ID
+router.put('/:productId', async (req, res, next) => {
+  const productId = req.params.productId;
+  const { name, description, image, price, quantity, categories, variants, sizes} = req.body;
+  try {
+    if (!name || !description || !price) {
+      next(ApiError.badRequest('Name, Description, and Price are required.'));
+      return;
+    }
+
+    const updateResult = await Product.updateOne({
+      _id: productId
+    }, {
+      $set: {
+        name,
+        description,
+        image,
+        categories,
+        price,
+        quantity,
+        variants,
+        sizes
+      }
+    });
+
+    if (updateResult.modifiedCount > 0) {
+      res.json({
+        _id: productId,
+        name,
+        description,
+        image,
+        categories,
+        price,
+        quantity,
+        variants,
+        sizes
+      });
+    } else {
+      next(ApiError.badRequest('No changes were made'))
+      return
+    }
+  } catch (error) {
+    next(ApiError.badRequest('Product not found'))
+    return
+  }
+});
 
 export default router
