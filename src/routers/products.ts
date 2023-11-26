f082d3cd2b3c05b63034c317da9deb03950933ce
unknown <ossama19992010@hotmail.com>
2023-11-26 18:10:55 +0300

    produts sorting and filtering added

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -4,28 +4,58 @@ const router = express.Router()
 import Product from '../models/product'
 import Order from '../models/order'
 import ApiError from '../errors/ApiError'
+import { Filter, SortOptions } from '../types/type'
 
 // get all products
 
 router.get('/', async (req, res) => {
   const page= Number(req.query.page) || 1
   const perPage = Number(req.query.perPage) || 3
-  const products = await Product.find().skip((page - 1) * perPage).limit(perPage).populate('categories')
-  const totalItems= await Product.countDocuments()
+  const category = req.query.category
+  const name = req.query.name
+  const price = req.query.price
+  const sort = req.query.sort
+
+  const filters: Filter ={}
+  const sortOptions:SortOptions = {}
+
+  if (category && typeof category === 'string'){
+    filters.categories= category
+  }
+  if (name && typeof name === 'string'){
+    filters.name = { $regex: new RegExp(name, 'i') };
+  }
+  // TODO: imporove this to accecpt any properties
+  if (sort && typeof sort === 'string'){
+    if(sort === 'asc'){
+      sortOptions.sort ={name: 1}
+    }
+    if(sort === 'desc'){
+      sortOptions.sort = {name: -1}
+    }
+  }
+
+  const products = await Product.find(filters)
+  .sort(sortOptions.sort)
+  .skip((page - 1) * perPage)
+  .limit(perPage)
+  .populate('categories')
+
+  const totalItems= await Product.countDocuments(filters)
   const totalPages = Math.ceil(totalItems / perPage)
-  console.log('products:', products)
+
   res.json({
+    page,
+    perPage,
     totalItems,
     totalPages,
-    perPage,
-    page,
     products
   })
 })
 // get single product by id
 router.get('/:productId', async (req, res, next) => {
   const productId = req.params.productId
-  const product = await Product.findById(productId).populate('categories')
+  const product = await Product.findById(productId)
   if(!product){
     next(ApiError.badRequest('Product not found!'))
   }
@@ -53,7 +83,7 @@ router.post('/', async (req, res, next) => {
   await product.save()
   res.json(product)
 })
-// Delete a product by its ID.
+// Delete a product by its ID
 router.delete('/:productId', async (req, res, next) => {
   const productId = req.params.productId
   const product =await Product.deleteOne({
