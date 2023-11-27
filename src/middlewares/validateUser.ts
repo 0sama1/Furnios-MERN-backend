463b0c75f52fd92238fae65a6162c28ed66113fe
Shahad Alshubrmi <Shahad.Alshubrmi@gmail.com>
2023-11-27 11:58:20 +0300

    users

---
+++ src/middlewares/validateUser.ts
@@ -0,0 +1,27 @@
+import zod, { ZodError } from 'zod'
+import { NextFunction, Request, Response } from 'express'
+
+import ApiError from '../errors/ApiError'
+
+export function validateUser(req: Request, res: Response, next: NextFunction) {
+  const schema = zod.object({
+    // firstName: zod.string(),
+    // lastName: zod.string(),
+    email: zod.string().email(),
+    password: zod.string().min(6),
+  })
+
+  try {
+    const validatedUser = schema.parse(req.body)
+    req.validatedUser = validatedUser
+    next()
+  } catch (error) {
+    const err = error
+    if (err instanceof ZodError) {
+      next(ApiError.badRequestValidation(err.errors))
+      return
+    }
+
+    next(ApiError.internal('Something went wrong'))
+  }
+}
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,27 +1,37 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+
 export type UserDocument = Document & {
   firstName: string
   lastName: string
   email: string
   password: string
   role: string
-  avatar:string
+  avatar: string
   isBlocked: boolean
+  isActive: boolean
+  activationToken: string | undefined
+}
+
+function validateRole(role: string) {
+  if (role === 'USER' || role === 'ADMIN') {
+    return true
+  }
+  return false
 }
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    required: true,
+    // required: true,
   },
   lastName: {
     type: String,
-    required: true,
+    // required: true,
   },
   email: {
     type: String,
     required: true,
     unique: true,
-
   },
   password: {
     type: String,
@@ -29,16 +39,23 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-
-    required: true,
+    default: 'USER',
+    validate: [validateRole, 'Role has to be either USER or ADMIN'],
   },
   avatar: {
     type: String,
   },
-  isBlocked:{
+  isBlocked: {
     type: Boolean,
+    default: false,
   },
-
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  activationToken: {
+    type: String,
+  },
 
   // relation between order and user should be many orders to one user
   // here's 1to1 just for the demo
@@ -48,4 +65,4 @@ const userSchema = new mongoose.Schema({
   },
 })
 
-export default mongoose.model('User', userSchema)
+export default mongoose.model<UserDocument>('User', userSchema)
