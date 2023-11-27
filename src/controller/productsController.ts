14db8cd6e9a56520a3e0d9665f151f1479a0818c
unknown <ossama19992010@hotmail.com>
2023-11-27 22:42:01 +0300

    Products controller added

--- src/controller/productsController.ts
+++ src/controller/productsController.ts
@@ -1,55 +1,141 @@
 import { NextFunction, Request, Response } from "express"
+import { Filter, SortOptions } from '../types/types'
 
-let products = [
-  { id: "1", title: "apple", price: 1234 },
-  { id: "2", title: "apple II", price: 123 },
-  { id: "2", title: "apple III", price: 123 },
-  { id: "2", title: "Apple iPhone", price: 123 },
-]
+import Product from '../models/product'
+import ApiError from "../errors/ApiError"
 
-export const getAllProducts = (req: Request, res: Response) => {
+
+
+export const getAllProducts = async (req: Request, res: Response) => {
+  const page= Number(req.query.page) || 1
+  const perPage = Number(req.query.perPage) || 3
+  const category = req.query.category
+  const name = req.query.name
+  const price = req.query.price
+  const sort = req.query.sort
+
+  const filters: Filter ={}
+  const sortOptions:SortOptions = {}
+
+  if (category && typeof category === 'string'){
+    filters.categories= category
+  }
+  if (name && typeof name === 'string'){
+    filters.name = { $regex: new RegExp(name, 'i') };
+  }
+  // TODO: imporove this to accecpt any properties
+  if (sort && typeof sort === 'string'){
+    if(sort === 'asc'){
+      sortOptions.sort ={name: 1}
+    }
+    if(sort === 'desc'){
+      sortOptions.sort = {name: -1}
+    }
+  }
+
+  const products = await Product.find(filters)
+  .sort(sortOptions.sort)
+  .skip((page - 1) * perPage)
+  .limit(perPage)
+  .populate('categories')
+
+  const totalItems= await Product.countDocuments(filters)
+  const totalPages = Math.ceil(totalItems / perPage)
+
   res.json({
-    message: "All the products returned",
-    payload: products,
+    page,
+    perPage,
+    totalItems,
+    totalPages,
+    products
   })
 }
 
-export const getProduct = (req: Request, res: Response, next: NextFunction) => {
-  const id = req.params.id
-  try {
-    const product = products.find((products) => products.id === id)
-    if (!product) {
-      const error = new Error("product is not found with this id...")
-      //@ts-expect-error
-      error.status = 404
-      throw error
-    }
-    res.status(200).json({
-      message: "single product found",
-      payload: product,
-    })
-  } catch (error) {
-    next(error)
+export const getProduct = async (req: Request, res: Response, next: NextFunction) => {
+  const productId = req.params.productId
+  const product = await Product.findById(productId)
+  if(!product){
+    next(ApiError.badRequest('Product not found!'))
   }
+  res.json(product)
 }
 
-export const deleteProduct = (req: Request, res: Response, next: NextFunction) => {
-  const id = req.params.id
-  const product = products.find((products) => products.id === id)
+export const createProduct = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, image, description, categories, variants, sizes, price, quantity } = req.body
+
+  if (!name || !description || !price) {
+    next(ApiError.badRequest('Name, Description and Price are requried'))
+    return
+  }
+  const product = new Product({
+    name,
+    image,
+    description, 
+    categories, 
+    variants, 
+    sizes, 
+    price, 
+    quantity
+  })
+
+  await product.save()
+  res.json(product)
+}
+
+export const deleteProduct = async (req: Request, res: Response, next: NextFunction) => {
+  const productId = req.params.productId
+  const product =await Product.deleteOne({
+    _id: productId
+  })
+  res.json({
+    _id: productId,
+    name: req.body.name
+  })
+
+}
+
+export const updateProduct = async (req: Request, res: Response, next:NextFunction) => {
+  const productId = req.params.productId;
+  const { name, description, image, price, quantity, categories, variants, sizes} = req.body;
   try {
-    if (!product) {
-      const error = new Error("product is not found with this id...")
-      //@ts-expect-error
-      error.status = 404
-      throw error
+    if (!name || !description || !price) {
+      next(ApiError.badRequest('Name, Description, and Price are required.'));
+      return;
     }
-    const filteredProduct = products.filter((product) => product.id !== id)
-    products= filteredProduct
-    res.status(200).json({
-      message: "product is deleted",
-      payload: product,
-    })
+
+    const updateResult = await Product.updateOne({
+      _id: productId
+    }, {
+      $set: {
+        name,
+        description,
+        image,
+        categories,
+        price,
+        quantity,
+        variants,
+        sizes
+      }
+    });
+
+    if (updateResult.modifiedCount > 0) {
+      res.json({
+        _id: productId,
+        name,
+        description,
+        image,
+        categories,
+        price,
+        quantity,
+        variants,
+        sizes
+      });
+    } else {
+      next(ApiError.badRequest('No changes were made'))
+      return
+    }
   } catch (error) {
-    next(error)
+    next(ApiError.badRequest('Product not found'))
+    return
   }
 }
--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,145 +1,25 @@
 import express from 'express'
 const router = express.Router()
 
-import Product from '../models/product'
+
 import Order from '../models/order'
 import ApiError from '../errors/ApiError'
-import { Filter, SortOptions } from '../types/types'
+import { createProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from '../controller/productsController'
+
 
 // get all products
 
-router.get('/', async (req, res) => {
-  const page= Number(req.query.page) || 1
-  const perPage = Number(req.query.perPage) || 3
-  const category = req.query.category
-  const name = req.query.name
-  const price = req.query.price
-  const sort = req.query.sort
+router.get('/', getAllProducts )
 
-  const filters: Filter ={}
-  const sortOptions:SortOptions = {}
-
-  if (category && typeof category === 'string'){
-    filters.categories= category
-  }
-  if (name && typeof name === 'string'){
-    filters.name = { $regex: new RegExp(name, 'i') };
-  }
-  // TODO: imporove this to accecpt any properties
-  if (sort && typeof sort === 'string'){
-    if(sort === 'asc'){
-      sortOptions.sort ={name: 1}
-    }
-    if(sort === 'desc'){
-      sortOptions.sort = {name: -1}
-    }
-  }
-
-  const products = await Product.find(filters)
-  .sort(sortOptions.sort)
-  .skip((page - 1) * perPage)
-  .limit(perPage)
-  .populate('categories')
-
-  const totalItems= await Product.countDocuments(filters)
-  const totalPages = Math.ceil(totalItems / perPage)
-
-  res.json({
-    page,
-    perPage,
-    totalItems,
-    totalPages,
-    products
-  })
-})
 // get single product by id
-router.get('/:productId', async (req, res, next) => {
-  const productId = req.params.productId
-  const product = await Product.findById(productId)
-  if(!product){
-    next(ApiError.badRequest('Product not found!'))
-  }
-  res.json(product)
-})
+router.get('/:productId', getProduct)
 
-router.post('/', async (req, res, next) => {
-  const { name, image, description, categories, variants, sizes, price, quantity } = req.body
+router.post('/', createProduct)
 
-  if (!name || !description || !price) {
-    next(ApiError.badRequest('Name, Description and Price are requried'))
-    return
-  }
-  const product = new Product({
-    name,
-    image,
-    description, 
-    categories, 
-    variants, 
-    sizes, 
-    price, 
-    quantity
-  })
-
-  await product.save()
-  res.json(product)
-})
 // Delete a product by its ID
-router.delete('/:productId', async (req, res, next) => {
-  const productId = req.params.productId
-  const product =await Product.deleteOne({
-    _id: productId
-  })
-  res.json({
-    _id: productId,
-    name: req.body.name
-  })
+router.delete('/:productId', deleteProduct)
 
-})
 // update a product by its ID
-router.put('/:productId', async (req, res, next) => {
-  const productId = req.params.productId;
-  const { name, description, image, price, quantity, categories, variants, sizes} = req.body;
-  try {
-    if (!name || !description || !price) {
-      next(ApiError.badRequest('Name, Description, and Price are required.'));
-      return;
-    }
-
-    const updateResult = await Product.updateOne({
-      _id: productId
-    }, {
-      $set: {
-        name,
-        description,
-        image,
-        categories,
-        price,
-        quantity,
-        variants,
-        sizes
-      }
-    });
-
-    if (updateResult.modifiedCount > 0) {
-      res.json({
-        _id: productId,
-        name,
-        description,
-        image,
-        categories,
-        price,
-        quantity,
-        variants,
-        sizes
-      });
-    } else {
-      next(ApiError.badRequest('No changes were made'))
-      return
-    }
-  } catch (error) {
-    next(ApiError.badRequest('Product not found'))
-    return
-  }
-});
+router.put('/:productId', updateProduct)
 
 export default router
